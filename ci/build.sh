e6b5b5ab6b6333180e2aeb0f73bc1308da321e4f
vsegu-qcom <vsegu@qti.qualcomm.com>
2026-09-25 14:10:32 +0530

    update build.sh file to pick qmi-headers

--- ci/build.sh
+++ ci/build.sh
@@ -43,6 +43,7 @@ RUN git clone --depth 1 --branch v0.1.4 https://github.com/qualcomm/qmi-framewor
     make -j"$(nproc)" && \
     make install && \
     ldconfig && \
+	  sed -i 's|^Cflags: .*|Cflags: -I${includedir} -I${includedir}/qmi_framework|' /usr/lib/pkgconfig/qmi-framework.pc && \
     cd / && rm -rf /tmp/qmi-framework
 
 # If the requested --host cross-compiler is not present, fall back to native build
